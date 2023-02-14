097a83322f947dd3829a5ff8496c5aa4577a6875
nikirana22 <nikirana1983@gmail.com>
2023-02-14 20:16:50 +0530

    refactor(main):add height , width global variable because of response design and call removeSystemBars method in main method (because we don't need system bar's )

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:foodei/screens/login_screen.dart';
 import 'package:foodei/utils/screen_tuils.dart';
 
-void main() {
-  runApp(
-  MaterialApp(
-    theme: ThemeData(primarySwatch: Colors.green),
-    debugShowCheckedModeBanner: false,
-  home:LoginScreen()));
+late double height;
+late double width;
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await ScreenUtils.removeSystemBars();
+  runApp(MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.green),
+      debugShowCheckedModeBanner: false,
+      home:const LoginScreen()));
 }
