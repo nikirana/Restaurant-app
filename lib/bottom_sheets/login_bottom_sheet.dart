569b974b2110978e736bc2f7820ad56c311e0dbd
nikirana22 <nikirana1983@gmail.com>
2023-02-15 10:41:53 +0530

    refactor(LoginBottomSheet): change LoginBottomSheet widget from stateful to stateless

--- lib/bottom_sheets/login_bottom_sheet.dart
+++ lib/bottom_sheets/login_bottom_sheet.dart
@@ -1,35 +1,32 @@
 import "package:flutter/material.dart";
+import 'package:foodei/main.dart';
 
-class LoginBottomSheet extends StatefulWidget {
+class LoginBottomSheet extends StatelessWidget {
   const LoginBottomSheet({super.key});
 
   @override
-  State<LoginBottomSheet> createState() => _LoginBottomSheetState();
-}
-
-class _LoginBottomSheetState extends State<LoginBottomSheet> {
-  @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: width*0.1),
+      height: height * 0.75,
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+              topRight: Radius.circular(height * 0.05),
+              topLeft: Radius.circular(height * 0.05))),
+      child: Column(
+        children:[
+          TextField(
               decoration: InputDecoration(
                   hintText: "Email",
                   border: OutlineInputBorder(
                       borderSide: BorderSide(
-                          width: 1, color: Color.fromARGB(232, 141, 243, 182)))
-                   ,enabledBorder:   OutlineInputBorder(
+                          width: 1, color: Color.fromARGB(232, 141, 243, 182))),
+                  enabledBorder: OutlineInputBorder(
                       borderSide: BorderSide(
-                          width: 1, color: Color.fromARGB(232, 141, 243, 182)))
-                        
-                ),
-            ),
-          ],
-        ),
+                          width: 1,
+                          color: Color.fromARGB(232, 141, 243, 182)))))
+        ],
       ),
     );
   }
