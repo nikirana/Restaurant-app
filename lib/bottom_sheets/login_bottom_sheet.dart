eaf9aab572af05c4104dcb326f8fd537398833bf
nikirana22 <nikirana1983@gmail.com>
2023-02-17 10:18:38 +0530

    refactor(login_bottom_sheet):  change file code (previous code(widgets,logic) was not correct )
    (Need to Review )

--- lib/bottom_sheets/login_bottom_sheet.dart
+++ lib/bottom_sheets/login_bottom_sheet.dart
@@ -1,287 +1,146 @@
 import "package:flutter/material.dart";
-import "package:font_awesome_flutter/font_awesome_flutter.dart";
 import "package:foodei/main.dart";
-import "package:foodei/screens/forget_password_screen.dart";
-import "package:foodei/utils/text_form_field.dart";
+import 'package:foodei/widgets/custom_page.dart';
 
 class LoginBottomSheet extends StatefulWidget {
-  const LoginBottomSheet({super.key});
+  final int index;
+  const LoginBottomSheet({required this.index,super.key});
 
   @override
   State<LoginBottomSheet> createState() => _LoginBottomSheetState();
 }
 
-class _LoginBottomSheetState extends State<LoginBottomSheet> {
-  @override
-  void changeCount() {
-    if (count == 1) {
-      count = 0;
-    } else {
-      count = 1;
-    }
-    setState(() {});
+class _LoginBottomSheetState extends State<LoginBottomSheet>
+    with SingleTickerProviderStateMixin {
+  final _nameEmailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _createAccountEmailController = TextEditingController();
+  late int _pageIndex = widget.index;
+  bool _allDetailsFilled=false;
+
+  late final TabController _tabController =
+  TabController(length: 2, vsync: this, initialIndex: _pageIndex);
+
+  void _makeControllerTextEmpty(){
+    _nameEmailController.text='';
+    _passwordController.text='';
   }
 
-  int count = 0;
-  var _emailController = TextEditingController();
-  var _passwordController = TextEditingController();
-  var _nameController = TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+    _tabController.addListener(() {
+      _makeControllerTextEmpty();
+      setState(() {
+        _allDetailsFilled=false;
+        _pageIndex = _tabController.index;
+      });
+    });
+  }
+  void _disposeAllControllers(){
+    _passwordController.dispose();
+    _nameEmailController.dispose();
+    _createAccountEmailController.dispose();
+    _tabController.removeListener(() { });
+    _tabController.dispose();
+  }
+
   @override
   void dispose() {
-    // TODO: implement dispose
     super.dispose();
-    _emailController.dispose();
-    _passwordController.dispose();
-    _nameController.dispose();
+    _disposeAllControllers();
   }
 
+  @override
   Widget build(BuildContext context) {
-    Size sz = MediaQuery.of(context).size;
-    height = sz.height;
-    width = sz.width;
-    return SafeArea(
-      child: Container(
-        height: height * 0.7,
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(height * .05),
-                topRight: Radius.circular(height * .05))),
-        child: Padding(
-          padding: EdgeInsets.all(width * 0.04),
-          child: ListView(children: [
-            SizedBox(
-              height: height * .01,
+    //todo when we are change the page the the next page text is showing very close to this page (fix that !!! )
+    return Container(
+      height: height * 0.7,
+      padding: EdgeInsets.symmetric(horizontal: width * 0.07),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(height * .04),
+              topRight: Radius.circular(height * .04))),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: height * .03,
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Container(
+              height: height * 0.01,
+              width: width * 0.15,
+              decoration: BoxDecoration(
+                  color: Colors.grey.shade300,
+                  borderRadius: BorderRadius.circular(height * 0.05)),
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: width * 0.08),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  GestureDetector(
-                    onTap: changeCount,
-                    child: Text(
-                      "Login",
-                      style: TextStyle(
-                          color: count == 0 ? Colors.green : Colors.black,
-                          fontSize: 18,
-                          fontWeight: FontWeight.w600,
-                          textBaseline: TextBaseline.ideographic),
-                    ),
-                  ),
-                  GestureDetector(
-                    onTap: changeCount,
-                    child: Text(
-                      "Create Account",
-                      style: TextStyle(
-                          color: count == 1 ? Colors.green : Colors.black,
-                          fontSize: 18,
-                          fontWeight: FontWeight.w600),
-                    ),
-                  ),
-                ],
-              ),
+          ),
+          SizedBox(height: height * 0.02),
+          //todo TabBar is not align (make the tabbar same as design)
+          Container(
+            height: height * 0.05,
+            width: width * 0.85,
+            alignment: Alignment.topLeft,
+            child: TabBar(
+              controller: _tabController,
+              tabs:const [
+                Tab(text: 'Create Account'),
+                Tab(
+                  text: "Login",
+                ),
+              ],
+              unselectedLabelColor: Colors.black,
+              labelColor: Colors.green,
+              indicatorSize: TabBarIndicatorSize.label,
             ),
-            SizedBox(
-              height: height * 0.02,
+          ),
+          Container(
+            // color: Colors.red,
+            height: height * 0.57,
+            child: TabBarView(
+              controller: _tabController,
+              children: [
+                _buildPage(0),
+                _buildPage(1)
+              ],
             ),
-            count == 0
-                ? Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Email Address",
-                        style: TextStyle(
-                            fontWeight: FontWeight.w500,
-                            fontSize: height * 0.016),
-                      ),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      TextFormFieldWidget(_emailController, false, "Email"),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Text(
-                        "Password",
-                        style: TextStyle(
-                            fontWeight: FontWeight.w500,
-                            fontSize: height * 0.016),
-                      ),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      TextFormFieldWidget(
-                          _passwordController, true, "Password"),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          GestureDetector(
-                            onTap: (){
-                              Navigator.push(context,MaterialPageRoute(builder: (context)=>ForgotPasswordScreen()));
-                            },
-                            child: Text(
-                              "Forgot Password?",
-                              style: TextStyle(color: Colors.green),
-                            ),
-                          )
-                        ],
-                      ),
-                      SizedBox(
-                        height: height * 0.02,
-                      ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: width * .04),
-                        child: Center(
-                            child: Container(
-                          height: height * 0.05,
-                          width: double.infinity,
-                          decoration: BoxDecoration(
-                              color: Colors.green,
-                              borderRadius:
-                                  BorderRadius.circular(height * 0.01)),
-                          child: Center(
-                              child: Text(
-                            "Login",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.w500,
-                                fontSize: 20),
-                          )),
-                        )),
-                      ),
-                      SizedBox(
-                        height: height * 0.02,
-                      ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: width * 0.04),
-                        child: Center(
-                            child: Container(
-                          height: height * 0.05,
-                          width: double.infinity,
-                          decoration: BoxDecoration(
-                              color: Color.fromARGB(255, 232, 230, 230),
-                              borderRadius:
-                                  BorderRadius.circular(height * 0.01)),
-                          child: Center(
-                              child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Icon(FontAwesomeIcons.google),
-                              SizedBox(width: width * 0.02),
-                              Text(
-                                "Login with Google",
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w500,
-                                    fontSize: height * 0.02),
-                              ),
-                            ],
-                          )),
-                        )),
-                      ),
-                    ],
-                  )
-                :
-                Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Full Name",
-                        style: TextStyle(
-                            fontWeight: FontWeight.w500,
-                            fontSize: height * 0.016),
-                      ),
-                      SizedBox(
-                        height: height * 0.005,
-                      ),
-                      TextFormFieldWidget(_nameController, false, "Full Name"),
-                      SizedBox(
-                        height: height * 0.01,
-                      ),
-                      Text(
-                        "Email Address",
-                        style: TextStyle(
-                            fontWeight: FontWeight.w500,
-                            fontSize: height * 0.016),
-                      ),
-                      SizedBox(
-                        height: height * 0.005,
-                      ),
-                      TextFormFieldWidget(_emailController, false, "Email"),
-                      SizedBox(
-                        height: height * 0.01,
-                      ),
-                      Text(
-                        "Password",
-                        style: TextStyle(
-                            fontWeight: FontWeight.w500,
-                            fontSize: height * 0.016),
-                      ),
-                      SizedBox(
-                        height: height * 0.005,
-                      ),
-                      TextFormFieldWidget(
-                          _passwordController, true, "Password"),
-                      SizedBox(
-                        height: height * 0.01,
-                      ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: width * 0.04),
-                        child: Center(
-                            child: Container(
-                          height: height * 0.05,
-                          width: double.infinity,
-                          decoration: BoxDecoration(
-                              color: Colors.green,
-                              borderRadius:
-                                  BorderRadius.circular(height * 0.01)),
-                          child: Center(
-                              child: Text(
-                            "Register",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.w500,
-                                fontSize: 20),
-                          )),
-                        )),
-                      ),
-                      SizedBox(
-                        height: width * 0.04,
-                      ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: width * 0.04),
-                        child: Center(
-                            child: Container(
-                          height: height * 0.05,
-                          width: double.infinity,
-                          decoration: BoxDecoration(
-                              color: Color.fromARGB(255, 232, 230, 230),
-                              borderRadius:
-                                  BorderRadius.circular(height * 0.01)),
-                          child: Center(
-                              child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Icon(FontAwesomeIcons.google),
-                              SizedBox(
-                                width: width * 0.02,
-                              ),
-                              Text(
-                                "Sign up with Google",
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w500,
-                                    fontSize: height * 0.02),
-                              ),
-                            ],
-                          )),
-                        )),
-                      ),
-                    ],
-                  )
-          ]),
-        ),
+          ),
+        ],
+      ),
+    );
+  }
+  bool _isDetailsFilled() {
+    return _passwordController.text.isNotEmpty &&
+        _nameEmailController.text.isNotEmpty &&
+        _pageIndex==1
+        ? true
+        : _createAccountEmailController.text.isNotEmpty;
+  }
+  void _onEditingComplete(){
+    print('onEditingComplete method is calling');
+    if(_isDetailsFilled()){
+      print('all details are complete');
+      setState(() {
+        _allDetailsFilled=true;
+      });
+    }
+  }
+
+  Widget _buildPage(int index) {
+    return SizedBox(
+      height: height*0.55,
+      child: CustomPage(
+        index: index,
+        allDetailsFilled: _allDetailsFilled,
+        onEditingComplete: _onEditingComplete,
+        nameEmailController: _nameEmailController,
+        passwordController: _passwordController,
+        createAccountEmailController: _pageIndex == 0
+            ? _createAccountEmailController
+            : null,
       ),
     );
   }
